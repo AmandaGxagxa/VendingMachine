9b7e36d761df46c6eb60088b1b83e816a85a9fad
amandagxagxa <amandagxagxa@gmail.com>
2020-05-19 09:53:05 +0200

    adding my constructor

--- src/Vending/OverloadedVendingMachine.java
+++ src/Vending/OverloadedVendingMachine.java
@@ -2,4 +2,8 @@ package Vending;
 
 public class OverloadedVendingMachine {
 
+    OverloadedVendingMachine(int softDrinkQty, int saltySnacksQty, int chocolatesQty){
+        // set the stockLevel instance variables for each product in the constructor
+
+    }
 }
