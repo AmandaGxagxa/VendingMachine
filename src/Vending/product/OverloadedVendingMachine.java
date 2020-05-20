7d563a7b655a2b68e38f258f39f33ca9af84c806
amandagxagxa <amandagxagxa@gmail.com>
2020-05-20 11:54:19 +0200

    fixed a constructor

--- src/Vending/product/OverloadedVendingMachine.java
+++ src/Vending/product/OverloadedVendingMachine.java
@@ -5,7 +5,7 @@ public class OverloadedVendingMachine {
     static int softDrink ;
     static int saltySnack;
     static int chocolate ;
-
+    int stockCount;
      //add a constructor like this to you OverloadedVendingMachine class
     OverloadedVendingMachine(int softDrinkQty, int saltySnacksQty, int chocolatesQty) {
         // set the stockLevel instance variables for each product in the constructor
@@ -13,25 +13,35 @@ public class OverloadedVendingMachine {
         this.saltySnack = saltySnacksQty;
         this.chocolate = chocolatesQty;
     }
+
+//    public OverloadedVendingMachine( ) {
+//
+   // }
+
     public void buy(SoftDrink softDrink) {
 
+        stockCount++;
         System.out.println("Bought soft drink");
     }
 
 
-    public void buy(SaltySnack saltySnack){
+    public void buy(SaltySnack saltySnack)
+    {
+        stockCount++;
         System.out.println("Bought SaltySnack");
     }
     public  void buy(Chocolate chocolate){
+        stockCount++;
         System.out.println("Bought SaltySnack");
     }
-    void buy() {
+    void buy(Product product) {
+        stockCount++;
         System.out.println("This is a product");
     }
 
     public static void main(String [] args){
-        //OverloadedVendingMachine ovm = new OverloadedVendingMachine();
-
+        OverloadedVendingMachine vendingMachine = new OverloadedVendingMachine();
+//Vending vending = new Vending()
         SoftDrink softDrink =  new SoftDrink();
         SaltySnack saltySnack =  new SaltySnack();
         softDrink.buy(new SoftDrink());
