c25e391fe26afc3b06563e02b022ef1e41dec39b
amandagxagxa <amandagxagxa@gmail.com>
2020-05-21 12:41:36 +0200

    added conditional statements

--- src/Vending/product/OverloadedVendingMachine.java
+++ src/Vending/product/OverloadedVendingMachine.java
@@ -1,77 +1,78 @@
 package Vending.product;
 
 public class OverloadedVendingMachine {
-    private  int chocolateCount;
-    private  int softDrinkCount ;
-    private  int saltySnackCount;
-    private  int productCount= chocolateCount + saltySnackCount + saltySnackCount;
+    private  int chocolatesQty;
+    private  int softDrinkQty ;
+    private  int saltySnacksQty;
+   // private  int productCount= chocolatesQty + softDrinkQty + saltySnacksQty;
 
     OverloadedVendingMachine(int softDrinkQty, int saltySnacksQty, int chocolatesQty) {
         // set the stockLevel instance variables for each product in the constructor
-        this.softDrinkCount= softDrinkQty;
-        this.saltySnackCount = saltySnacksQty;
-        this.chocolateCount = chocolatesQty;
+        this.softDrinkQty= softDrinkQty;
+        this.saltySnacksQty = saltySnacksQty;
+        this.chocolatesQty = chocolatesQty;
     }
 
     public void buy(SoftDrink softDrink) {
-
-        softDrinkCount--;
-        System.out.println("Bought soft drink " + this.softDrinkCount);
-    }
-
-    void buy(SaltySnack saltySnack)
-    {
-        saltySnackCount--;
-        System.out.println("Bought SaltySnack "+ this.saltySnackCount);
-        //return this.stockCount;
-    }
-    void buy(Chocolate chocolate){
-        chocolateCount--;
-        System.out.println("Bought Chocolate "+ this.chocolateCount);
-    }
-    //counte methods
-    public int getSoftDrinkCount(SoftDrink softDrink) {
-
-        return softDrinkCount--;
+        if(softDrinkQty>=1){
+            System.out.println("Bought soft drink " + this.softDrinkQty);
+            softDrinkQty--;
+        }
+        else
+        {
+            System.out.println("We are running out of soft drinks");
+        }
 
     }
+    public void buy(SaltySnack saltySnack) {
+        if(saltySnacksQty>=1){
+            System.out.println("Bought soft drink " + this.saltySnacksQty);
+            saltySnacksQty--;
+        }
+        else
+        {
+            System.out.println("We are running out of Salty Snacks");
+        }
 
-    public int getSaltySnackCount(SaltySnack saltySnack)
-    {
-      return   saltySnackCount--;
+    }
+    public void buy(Chocolate chocolate) {
+        if(chocolatesQty>=1){
+            System.out.println("Chocolate  is bought " + this.chocolatesQty);
+            chocolatesQty--;
+        }
+        else
+        {
+            System.out.println("We are running out of Chocolates");
+        }
 
+    }
+
+    public int getSaltySnackCount(SoftDrink softDrink) {
+        return   softDrinkQty--;
+    }
+
+    public int getSaltySnackCount(SaltySnack saltySnack) {
+      return   saltySnacksQty--;
     }
     public int getChocolateCount(Chocolate chocolate){
-       return chocolateCount--;
-        //System.out.println("Bought Chocolate "+ this.chocolateCount);
+       return chocolatesQty--;
+    }
 
-    }
-    public int getStock(Product product) {
-       final int products = chocolateCount + saltySnackCount + saltySnackCount;
-//        if (product instanceof SoftDrink){
-//
-//        }
-        System.out.println("This is a product " + products);
-        return products;
-    }
 
     //addStock METHOD
     void addStock(SoftDrink softDrink){
-        System.out.println("Softdrink added "+ this.softDrinkCount);
-        softDrinkCount++;
+        System.out.println("Softdrink added "+ this.softDrinkQty);
+        softDrinkQty++;
     }
     void addStock(SaltySnack saltySnack){
-        System.out.println("Salty snack added "+ this.saltySnackCount);
-      ++  saltySnackCount;
+        System.out.println("Salty snack added "+ this.saltySnacksQty);
+      ++  saltySnacksQty;
     }
     void addStock(Chocolate chocolate){
-        System.out.println("Chocolates added "+ this.chocolateCount);
-       ++ chocolateCount;
+        System.out.println("Chocolates added "+ this.chocolatesQty);
+       ++ chocolatesQty;
     }
-    public int addChocolate(Chocolate chocolate){
-      //  System.out.println(chocolateCount);
-    return chocolateCount;
-    }
+
 
 
     public static void main(String [] args){
@@ -82,13 +83,13 @@ public class OverloadedVendingMachine {
         ovm.addStock(new SoftDrink());
         ovm.addStock(new SoftDrink());
         ovm.buy(new Chocolate());
-        ovm.addChocolate(new Chocolate());
+
         ovm.buy(new SaltySnack());
         ovm.addStock(new SaltySnack());
         ovm.addStock(new SaltySnack());
         ovm.addStock(new SaltySnack());
 
-        ovm.getStock(new Product());
+        //ovm.getStock(new Product());
     }
 
 }
